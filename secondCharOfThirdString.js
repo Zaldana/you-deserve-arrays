325f217af22fd836157c99b509270eba6fb1273b
Zaldana <rooben.zaldana@gmail.com>
2021-06-08 11:03:30 -0400

    last problem upload

--- secondCharOfThirdString.js
+++ secondCharOfThirdString.js
@@ -4,33 +4,27 @@ const prompt = require('prompt-sync')({sigint: true});
 
 // User Prompt ===============================
 
-let userArray = prompt('Input an array of strings   ');
+const userArray = prompt('Input an array of strings   ');
 
 // Convert String Into an Object =============
 
-userArray = JSON.parse(userArray);
+const array = JSON.parse(userArray);
 
 // Character log =============================================
 
-let itemThree = userArray[2];
+const itemThree = array[2];
 
-console.log(itemThree);
-
-console.log(typeof itemThree);
-
-characters = JSON.parse(itemThree);
-
-console.log(typeof characters);
 
 // Is the first item a Number Log ============================
 
-//if (typeof characters[1] === "string") {
+if (typeof itemThree === 'string') {
 
+    const secondCharacter = itemThree[1];
+    console.log(secondCharacter);
+}
 
- //   console.log("string");
+else {
 
-//} else {
+    console.log("ERROR");
 
- //   console.log("error");
-
-//}
+}
